c1bae49d7f5231179ee94ae5ec91126eb55925f4
pochan <5851382@qq.com>
2015-01-21 11:11:02 +0800

    delete the device information verification

--- custom_app.sh
+++ custom_app.sh
@@ -10,5 +10,12 @@ if [ "$apkBaseName" = "Phone" ];then
     sed -i 's/Landroid\/app\/NotificationPlus/Lcom\/mediatek\/notification\/NotificationPlus/g' $tempSmaliDir/smali/com/android/phone/NotificationMgr.smali
     sed -i 's/Landroid\/app\/NotificationManagerPlus/Lcom\/mediatek\/notification\/NotificationManagerPlus/g' $tempSmaliDir/smali/com/android/phone/NotificationMgr.smali
 
+elif [ "$apkBaseName" = "Settings" ];then
+    echo ">>> in custom_app $apkBaseName"
+    sed -i '/com.android.settings.ManageApplicationsSettings/r Settings/settings_headers.xml.part' $tempSmaliDir/res/xml/settings_headers.xml	
+# Remove the Official Verify Preference.
+	sed -i -e "/^\.method.*setOfficialVerifyPreference()V/,/^\.end method/d" $tempSmaliDir/smali/com/android/settings/DeviceInfoSettings.smali
+	sed -i -e "/setOfficialVerifyPreference/d" $tempSmaliDir/smali/com/android/settings/DeviceInfoSettings.smali	
+
 fi
 
